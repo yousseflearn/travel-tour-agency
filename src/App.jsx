313cb516481acf2ab2009d7108a326fffd6aa023
yousseflearn <youssefeasylearn@gmail.com>
2025-01-16 13:03:05 -0500

    Create Hero Header Component with Responsive Mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import HeroHeader from "./components/hero-header/HeroHeader";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HeroHeader />
     </div>
   );
 }
