05d38c41d4c4208e87f41e49e81e08cee7fac62c
yousseflearn <youssefeasylearn@gmail.com>
2025-01-19 12:14:09 -0500

    Creation of Services Components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./components/header/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HeroHeader from "./components/hero-header/HeroHeader";
+import Services from "./components/services/Services";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <HeroHeader />
+      <Services />
     </div>
   );
 }
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,26 @@
+import "./services.css";
+
+const Services = () => {
+  return (
+    <div className="services">
+      <div className="service-item">
+        <i className="bi bi-card-image service-icon"></i>
+        Adventures
+      </div>
+      <div className="service-item">
+        <i className="bi bi-people-fill service-icon"></i>
+        Family Tour
+      </div>
+      <div className="service-item">
+        <i className="bi bi-building service-icon"></i>
+        City Card
+      </div>
+      <div className="service-item">
+        <i className="bi bi-globe service-icon"></i>
+        multi-days Trips
+      </div>
+    </div>
+  );
+};
+
+export default Services;
