bb175c444d7cfaa9d1a908a84d4b964cfad21360
yousseflearn <youssefeasylearn@gmail.com>
2025-02-04 13:47:23 -0500

    creation the  Pagination

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,27 @@ import HeroHeader from "./components/hero-header/HeroHeader";
 import Services from "./components/services/Services";
 import TourList from "./components/tour/TourList";
 import { toursList } from "./data";
+import Pagination from "./components/pagination/Pagination";
+import { useState } from "react";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const TOUR_PER_PAGE = 6;
+  const pages = Math.ceil(toursList.length / TOUR_PER_PAGE);
+  const startIndex = (currentPage - 1) * TOUR_PER_PAGE;
+  const finishIndex = currentPage * TOUR_PER_PAGE;
+  const tours = toursList.slice(startIndex, finishIndex);
   return (
     <div className="App">
       <Header />
       <HeroHeader />
       <Services />
-      <TourList toursList={toursList} />
+      <TourList toursList={tours} />
+      <Pagination
+        pages={pages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </div>
   );
 }
